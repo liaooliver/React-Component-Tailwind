4e1d586f9c99f109429d2d93dcff4ee7e7a80a8b
Oliver Liao <rtw0913@gmail.com>
2020-07-23 14:11:03 +0800

    contact in header, icon finish

--- src/components/ContactHeader.jsx
+++ src/components/ContactHeader.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import mail from '../assets/icon_mail.svg';
+import tel from '../assets/icon_tel.svg';
 
 const ContactHeader = () => (
-  <div className="w-full p-6 md:p-4 sm:p-2 pr-48 bg-gray-300 flex justify-between items-center">
+  <div className="w-full relative p-6 pr-48 py-10 md:px-4 sm:px-2 bg-gray-300 flex justify-between items-center">
     <span className="w-56 lg:w-48 md:w-32 sm:w-24">
       <img width="100%" src="https://readycrew.jp/assets/images/common/hd_logo.png" alt="" />
     </span>
@@ -15,7 +17,38 @@ const ContactHeader = () => (
       </ul>
     </nav>
     {/* icon btn */}
-    {/* <div></div> */}
+    <div className="fixed right-0 flex items-center z-10 mr-6">
+      <a href="/#">
+        <div className="w-20 h-20 mr-3 bg-red-600 hover:bg-white text-white hover:text-black rounded-full border border-red-700 flex flex-col items-center justify-center">
+          <span className="w-8 mb-1">
+            <img src={mail} alt="mail" />
+          </span>
+          <span className="text-xs">
+            お問い合わせ
+          </span>
+        </div>
+      </a>
+      <a href="/#">
+        <div className="xxl:hidden w-20 h-20 mr-3 bg-red-600 text-white rounded-full border border-red-700 flex flex-col items-center justify-center">
+          <span className="w-8 mb-1">
+            <img src={tel} alt="tel" />
+          </span>
+          <span className="text-xs">
+            電話
+          </span>
+        </div>
+      </a>
+      {/* menu btn */}
+      <div className="p-2 group flex flex-col items-center justify-center">
+        <div className="w-8 h-8 flex flex-col justify-around group">
+          <span className="w-full h-1 rounded-sm bg-black group-hover:bg-red-600 group-hover:w-full transition-all duration-200" />
+          <span className="w-1/3 h-1 rounded-sm bg-black group-hover:bg-red-600 group-hover:w-full transition-all duration-200" />
+          <span className="w-2/3 h-1 rounded-sm bg-black group-hover:bg-red-600 group-hover:w-full transition-all duration-200" />
+        </div>
+        <span className="text-xs font-bold whitespace-no-wrap group-hover:text-red-600 transition-all duration-200">メニュー</span>
+      </div>
+      {/* </div> */}
+    </div>
   </div>
 );
 
