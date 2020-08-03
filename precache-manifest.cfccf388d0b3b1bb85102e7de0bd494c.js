a288e830fc3325382174d981be2e6b8edb29d654
OliverLiao <rtw0913@gmail.com>
2020-08-03 20:30:39 +0800

    Updates

---
+++ precache-manifest.cfccf388d0b3b1bb85102e7de0bd494c.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4fa65e63d074bc9d91d5d56cfc92e7",
+    "url": "/React-Component-Tailwind/index.html"
+  },
+  {
+    "revision": "53256073bc6915efc7c6",
+    "url": "/React-Component-Tailwind/static/css/main.4b704634.chunk.css"
+  },
+  {
+    "revision": "f952d0642b4502f943c9",
+    "url": "/React-Component-Tailwind/static/js/2.0d605105.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Component-Tailwind/static/js/2.0d605105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53256073bc6915efc7c6",
+    "url": "/React-Component-Tailwind/static/js/main.f40437dd.chunk.js"
+  },
+  {
+    "revision": "df21327d7899a14d0872",
+    "url": "/React-Component-Tailwind/static/js/runtime-main.2d1af6bf.js"
+  },
+  {
+    "revision": "b5f8ef0cb10cf4fa6dfc584d29908421",
+    "url": "/React-Component-Tailwind/static/media/NotoSansJP-Regular.b5f8ef0c.otf"
+  },
+  {
+    "revision": "3cf8fe2c73c8d166630f1978a40cc67b",
+    "url": "/React-Component-Tailwind/static/media/arrow-hover.3cf8fe2c.svg"
+  },
+  {
+    "revision": "2fd54d28cfbb870e67b2d59e0fa8b1b9",
+    "url": "/React-Component-Tailwind/static/media/arrow.2fd54d28.svg"
+  },
+  {
+    "revision": "4c5d1ef26b299782edc4fc611febcba0",
+    "url": "/React-Component-Tailwind/static/media/facebook.4c5d1ef2.svg"
+  },
+  {
+    "revision": "06d24dd10e64217c164e1695aecff544",
+    "url": "/React-Component-Tailwind/static/media/gmail.06d24dd1.svg"
+  },
+  {
+    "revision": "7a823488126501292f0d87ebdaadf6aa",
+    "url": "/React-Component-Tailwind/static/media/icon_mail.7a823488.svg"
+  },
+  {
+    "revision": "8194a4198c30f8998b450c62bbe48765",
+    "url": "/React-Component-Tailwind/static/media/icon_tel.8194a419.svg"
+  },
+  {
+    "revision": "4d3c1369985d2ee11ea94727b8c0381a",
+    "url": "/React-Component-Tailwind/static/media/maxresdefault.4d3c1369.jpg"
+  },
+  {
+    "revision": "870a5694b10e508084d7ae1a39c1f665",
+    "url": "/React-Component-Tailwind/static/media/returnicon.870a5694.svg"
+  },
+  {
+    "revision": "c9f1200836515fd4bacd29160be78732",
+    "url": "/React-Component-Tailwind/static/media/twitter.c9f12008.svg"
+  }
+]);
