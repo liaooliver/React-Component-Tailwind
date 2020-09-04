bb931d3828462dfdbb3843235bb9587a8b4d2b81
Oliver Liao <rtw0913@gmail.com>
2020-09-04 14:22:22 +0800

    access context

--- src/components/hooks/BookList.jsx
+++ src/components/hooks/BookList.jsx
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { ThemeContext } from './contexts/ThemeContext';
 
 class BookList extends Component {
+
+    // Class.contextType
+    static contextType = ThemeContext
+
     render() { 
+
+        const { isLightTheme, light, dark } = this.context;
+        const theme = isLightTheme ? light : dark;
+
         return ( 
-            <div>
+            <div className="p-6 transition-all duration-700" style={{ color:theme.syntax, backgroundColor: theme.bg }}>
                 <ul>
-                    <li className="mb-4">The way of kings</li>
-                    <li className="mb-4">The name of the wind</li>
-                    <li className="mb-4">The final of empire</li>
+                    <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The way of kings</li>
+                    <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The name of the wind</li>
+                    <li className="p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The final of empire</li>
                 </ul>
             </div>
         );
--- src/components/hooks/NavBar.jsx
+++ src/components/hooks/NavBar.jsx
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
+import { ThemeContext } from './contexts/ThemeContext';
 
 class NavBar extends Component {
-    state = {  }
-    render() { 
+    render() {
+        // Context.Consumer
         return ( 
-            <nav className="p-2 mb-3">
-                <h1>Context App</h1>
-                <ul>
-                    <li className="inline-block m-3 rounded-md p-3">Home</li>
-                    <li className="inline-block m-3 rounded-md p-3">About</li>
-                    <li className="inline-block m-3 rounded-md p-3">Contact</li>
-                </ul>
-            </nav>
+            <ThemeContext.Consumer>
+                {
+                    (context) => {
+                        const { isLightTheme, light, dark } = context;
+                        const theme = isLightTheme ? light : dark;
+                        return <nav className="p-2 transition-all duration-700" style={{ backgroundColor: theme.ui, color: theme.syntax }}>
+                        <h1>Context App</h1>
+                        <ul>
+                            <li className="inline-block m-3 rounded-md p-3">Home</li>
+                            <li className="inline-block m-3 rounded-md p-3">About</li>
+                            <li className="inline-block m-3 rounded-md p-3">Contact</li>
+                        </ul>
+                    </nav>
+                    }
+                }
+            </ThemeContext.Consumer>
          );
     }
 }
