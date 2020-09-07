3577caa4992f40c7978d2edbf1624f29da1614b5
Oliver Liao <rtw0913@gmail.com>
2020-09-07 15:36:19 +0800

    hook project - 2, book list and remove book

--- src/components/reading-list/Book.jsx
+++ src/components/reading-list/Book.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import BookContextProvider from './context/BookContext';
 import NavBar from './components/NavBar';
+import BookList from './components/BookList';
 
 const Book = () => {
     return (
         <div className="p-10 bg-green-900 mx-auto">
             <BookContextProvider>
                 <NavBar />
+                <BookList />
             </BookContextProvider>
         </div>
     );
