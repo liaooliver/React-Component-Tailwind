8419237085e425de29605545ae3705c604f13822
Oliver Liao <rtw0913@gmail.com>
2020-09-07 14:08:46 +0800

    multiple context with hook

--- src/components/hooks/BookList.jsx
+++ src/components/hooks/BookList.jsx
@@ -1,26 +1,42 @@
-import React, { Component } from 'react';
+import React, { useContext } from 'react';
 import { ThemeContext } from './contexts/ThemeContext';
 
 
-class BookList extends Component {
+// class BookList extends Component {
 
-    // Class.contextType
-    static contextType = ThemeContext
-    render() { 
+//     // Class.contextType      only class component
+//     static contextType = ThemeContext
+//     render() { 
 
-        const { isLightTheme, light, dark } = this.context;
-        const theme = isLightTheme ? light : dark;
+//         const { isLightTheme, light, dark } = this.context;
+//         const theme = isLightTheme ? light : dark;
 
-        return ( 
-            <div className="p-6 transition-all duration-700" style={{ color:theme.syntax, backgroundColor: theme.bg }}>
-                <ul>
-                    <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The way of kings</li>
-                    <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The name of the wind</li>
-                    <li className="p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The final of empire</li>
-                </ul>
-            </div>
-        );
-    }
+//         return ( 
+//             <div className="p-6 transition-all duration-700" style={{ color:theme.syntax, backgroundColor: theme.bg }}>
+//                 <ul>
+//                     <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The way of kings</li>
+//                     <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The name of the wind</li>
+//                     <li className="p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The final of empire</li>
+//                 </ul>
+//             </div>
+//         );
+//     }
+// }
+
+const BookList = () => {
+
+    const { isLightTheme, light, dark } = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+
+    return ( 
+        <div className="p-6 transition-all duration-700" style={{ color:theme.syntax, backgroundColor: theme.bg }}>
+            <ul>
+                <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The way of kings</li>
+                <li className="mb-4 p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The name of the wind</li>
+                <li className="p-3 rounded-lg" style={{ backgroundColor: theme.ui }}>The final of empire</li>
+            </ul>
+        </div>
+    );
 }
  
 export default BookList;
